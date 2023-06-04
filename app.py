519cd671cd0ca4babba807921289ecc428caa079
Lasith-Niro <lasithniro.online@gmail.com>
2023-06-04 01:33:06 +0100

    UI updated + Tags added

--- app.py
+++ app.py
@@ -36,28 +36,35 @@ def index():
         
         way_history = fetch_way_history(way_id, start_date, end_date)
         # save way_history to a file
-        # with open('way_history.json', 'w') as f:
-        #     json.dump(way_history, f)
+        with open('way_history.json', 'w') as f:
+            json.dump(way_history, f)
 
                 
         # Extract the coordinates from the JSON response
         coordinates = []
+        n=0
         if way_history:
             elements = way_history["elements"]
             # print(json.dumps(elements, indent=4, sort_keys=True))
             for element in elements:
                 if element["type"] == "way":
+                    n+=1
                     geometry = element["geometry"]
                     version = element["version"]
                     timestamp = element["timestamp"]
                     user = element["user"]
+                    # if element["tags"] exists, then pass it otherwise pass an empty dict
+                    tags = element.get("tags", {})
+                    
+                    
                     # print("Geometry:", geometry)
-                    print("Version:", version)
-                    print()
-                    coordinates.append({"geometry": geometry, "version": version, "timestamp": timestamp, "user": user})
+                    # print("Version:", version)
+                    # print()
+                    coordinates.append({"geometry": geometry, "version": version, "timestamp": timestamp, "user": user, "tags": tags})
+            print("Number of versions:", n)
             print("-------------------")
             
-        print(coordinates)
+        # print(coordinates)
         # Render the template with the map and way history
         return render_template('map.html', coordinates=json.dumps(coordinates), way_id=way_id)
     
